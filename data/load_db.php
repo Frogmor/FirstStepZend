399ddd5b88fab298f2840ff1e39766e985dfa580
DESKTOP-SLQ2BMS\Valentin <haehnelval@gmail.com>
2017-06-07 12:59:49 +0200

    Premier pas avec Zend

---
+++ data/load_db.php
@@ -0,0 +1,9 @@
+<?php
+//Creation d'une base de données
+$db = new PDO('sqlite:' . realpath(__DIR__) . '/zftutorial.db');
+$fh = fopen(__DIR__ . '/schema.sql', 'r');
+while ($line = fread($fh, 4096)) {
+    $db->exec($line);
+}
+fclose($fh);
+ ?>
---
+++ module/Album/src/module.php
@@ -0,0 +1,30 @@
+<?php
+namespace Album;
+
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+    // getConfig() and getServiceConfig methods are here
+    public function getConfig()
+    {
+      return include __DIR__.'/../config/module.config.php';
+    }
+    // Add this method:
+    public function getControllerConfig()
+    {
+        return [
+            'factories' => [
+                Controller\AlbumController::class => function($container) {
+                    return new Controller\AlbumController(
+                        $container->get(Model\AlbumTable::class)
+                    );
+                },
+            ],
+        ];
+    }
+}
+?>
